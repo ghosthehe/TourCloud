1b09602eafd1bbcf1c52ba53e989282cb6644ef6
mainidea <mainidea@hotmail.com>
2016-06-08 17:21:09 +0800

    首页开发
    Signed-off-by: mainidea <mainidea@hotmail.com>

--- TourCloud/mainPage/model/TCMainPageRequestModel.m
+++ TourCloud/mainPage/model/TCMainPageRequestModel.m
@@ -16,7 +16,6 @@
 {
     if (self = [super init]) {
         
-        [self mainPageHotAndTypes];
         
         __weak typeof(self) weakSelf = self;
         
@@ -40,25 +39,61 @@
 
 - (NSArray *)parseDataWithComments:(NSArray *)comments
 {
-    NSMutableArray *items = [[NSMutableArray alloc] initWithCapacity:0];
-    TCMainPageParserItem *item = [[TCMainPageParserItem alloc] init];
-    [items addObject:item];
+    NSMutableArray *datas = [[NSMutableArray alloc] initWithCapacity:0];
     
-    return items;
+    NSArray *items = [TCMainPageParserItem parseArray:comments];
+    
+    for (TCMainPageParserItem *item in items) {
+        TCMainPageCellModel *cellModel = [[TCMainPageCellModel alloc] init];
+        [cellModel parseItem:item];
+        
+        [datas addObject:cellModel];
+    }
+    
+    return datas;
 }
 
-- (void)mainPageHotAndTypes
+- (void)defaultDatasource:(NSArray *)datas
 {
-    MSMutableDataSource *ds = [[MSMutableDataSource alloc] init];
+    //datasource
+    MSMutableDataSource *dataSource = [[MSMutableDataSource alloc] init];
     
     NSMutableArray *items = [[NSMutableArray alloc] initWithCapacity:0];
     TCMainPageParserItem *item = [[TCMainPageParserItem alloc] init];
     [items addObject:item];
+    [dataSource addNewSection:@"" withItems:items];
     
-    [ds addNewSection:@"" withItems:items];
+    [dataSource addNewSection:@"最新活动" withItems:datas];
+    self.dataSource = dataSource;
     
-    self.dataSource = ds;
+}
 
+- (void)pageDownDatasource:(NSArray *)datas
+{
+    //datasource
+    if (datas && datas.count)
+    {
+        [self.dataSource appendItems:datas atSectionTitle:@"最新活动"];
+    }
+    
+}
+
+- (void)pageUpDatasource:(NSArray *)datas
+{
+    if (datas && datas.count) {
+        
+        MSMutableDataSource *dataSource = [[MSMutableDataSource alloc] init];
+        
+        NSMutableArray *items = [[NSMutableArray alloc] initWithCapacity:0];
+        TCMainPageParserItem *item = [[TCMainPageParserItem alloc] init];
+        [items addObject:item];
+        [dataSource addNewSection:@"" withItems:items];
+        
+        [dataSource addNewSection:@"最新活动" withItems:datas];
+        self.dataSource = dataSource;
+        
+    }
+    
 }
 
 @end
--- TourCloud/mainPage/view/MainPageHotCell.m
+++ TourCloud/mainPage/view/MainPageHotCell.m
@@ -8,18 +8,69 @@
 
 #import "MainPageHotCell.h"
 #import "TCMainPageParserItem.h"
+#import "TCMainPageTypeCollectionCell.h"
+
+@interface MainPageHotCell ()<UICollectionViewDelegateFlowLayout,UICollectionViewDelegate>
+
+@property (nonatomic, strong) NSArray *typeTitles;
+@property (nonatomic, strong) NSArray *typeImages;
+
+@end
 
 @implementation MainPageHotCell
 
 - (void)awakeFromNib {
     // Initialization code
+    
+    UICollectionViewFlowLayout *flowLayout= [[UICollectionViewFlowLayout alloc]init];
+    flowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    flowLayout.minimumLineSpacing = 4;
+
+    self.typesCollection.collectionViewLayout = flowLayout;
+    
+    [self.typesCollection registerNib:[UINib nibWithNibName:@"TCMainPageTypeCollectionCell" bundle:nil] forCellWithReuseIdentifier:@"TCMainPageTypeCollectionCell"];
+    
 }
 
 - (void)update:(id<MSCellModel>)cellModel
 {
     if ([cellModel isKindOfClass:[TCMainPageParserItem class]]) {
         
+        self.typeTitles = @[@"线路", @"场馆", @"服务", @"景点", @"活动"];
+        self.typeImages = @[@"乐游上海1_07", @"乐游上海1_09", @"乐游上海1_11", @"乐游上海1_13", @"乐游上海1_20"];
+        
     }
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(85, 85);
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return [self.typeTitles count];
+}
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    TCMainPageTypeCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"TCMainPageTypeCollectionCell" forIndexPath:indexPath];
+
+    cell.typeTitleLabel.text = self.typeTitles[indexPath.row];
+    cell.typeTitleImage.image = [UIImage imageNamed:self.typeImages[indexPath.row]];
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
